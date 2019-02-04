bc43ff36485d793ce4a52e728e9e40cd3fd05e28
Fabiano Góes <fabianogoes@gmail.com>
2019-02-04 19:20:12 -0200

    initial commit

---
+++ src/main/java/com/eprogramar/demorepositoryrestresource/DemoRepositoryRestResourceApplication.java
@@ -0,0 +1,14 @@
+package com.eprogramar.demorepositoryrestresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRepositoryRestResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRepositoryRestResourceApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/eprogramar/demorepositoryrestresource/Person.java
@@ -0,0 +1,21 @@
+package com.eprogramar.demorepositoryrestresource;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String firstName;
+    private String lastName;
+
+}
